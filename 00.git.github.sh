9ec41efabb0ae92f36e842898d189b82ace4ed4e
jackieyihkb <jackieyihkb@ust.hk>
2024-06-21 11:03:53 +0800

    -

---
+++ 00.git.github.sh
@@ -0,0 +1,10 @@
+###################git add documents##################
+
+git clone git@github.com:jackieyihkb/BasicCommands.git
+cd BasicCommands/
+git init
+git add comparative_genomic_analysis.sh 
+git commit -m "first commands"
+git remote add origin git@github.com:jackieyihkb/BasicCommands.git
+git remote -v
+git push -u origin master
