d75f61b03caa657ef0be7f318ba1cf923d0c25d9
jackieyihkb <jackieyihkb@ust.hk>
2024-06-24 10:42:48 +0800

    commit

---
+++ 00.database.sh
@@ -0,0 +1,16 @@
+Some reference database:
+1. PPGR, https://ngdc.cncb.ac.cn/ppgr/
+2. sapbase, http://www.sapindaceae.com/index.html#
+3. InsectBase 2.0, http://v2.insect-genome.com/
+4. PhylomeDB 5, http://www.phylomedb.org/
+5. ChlamDB, https://chlamdb.ch/home/#home
+6. CottonMD, https://yanglab.hzau.edu.cn/CottonMD.1
+7. IAnimal, https://ianimal.pro/index
+8. EDomics, http://edomics.qnlm.ac/#/home
+9. MolluscDB, http://mgbase.qnlm.ac/home
+
+Can be considered for publication:
+1. Nucleic Acids Research
+2. Genomics, Proteomics & Bioinformatics
+3. Journal of Integrative Plant Biology
+4. Journal of Genetics and Genomics
